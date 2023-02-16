a26137f6e6a0db34a736c5c1c376119f2c0fc24c
AliAngelinaaa <nanzum1140@gmail.com>
2023-02-15 21:19:50 -0500

    this should be the basic but idk yet

---
+++ medical-bills/script.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+let bills = [];
+
+app.use(bodyParser.json());
+
+//Get Items
+app.get('/items',(req, res)=> {
+    res.json(bills);
+})
+
+//POst items
+app.post('/items', (req, res) => {
+    const{patientName, patientAddress, hospitalName, dateOfService, billAmount} = req.body;
+    const newBill = {patientName, patientAddress, hospitalName, dateOfService, billAmount};
+    bills.push(newBill);
+    res.status(201).json(newBill);
+})
+
+//server start
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT $(PORT)`);
+})
